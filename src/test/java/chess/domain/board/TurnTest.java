ae203202aa55af8cd80a96fdd7899f9a4378ba4e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-29 00:46:29 +0900

    test(Turn): 해당 색상의 팀 차례인지 확인하는 테스트 추가

--- src/test/java/chess/domain/board/TurnTest.java
+++ src/test/java/chess/domain/board/TurnTest.java
@@ -1,6 +1,7 @@
 package chess.domain.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.domain.piece.Color;
 import org.junit.jupiter.api.DisplayName;
@@ -12,5 +13,15 @@ class TurnTest {
     @DisplayName("게임 시작 시 턴은 검정팀부터 시작한다.")
     void createTurn() {
         assertThat(new Turn().isTurn(Color.BLACK)).isTrue();
+    }
+
+    @Test
+    @DisplayName("해당 색상의 팀 차례인지 확인한다.")
+    void isTurn() {
+        Turn blackTurn = new Turn();
+        assertAll(
+                () -> assertThat(blackTurn.isTurn(Color.BLACK)).isTrue(),
+                () -> assertThat(blackTurn.isTurn(Color.WHITE)).isFalse()
+        );
     }
 }
