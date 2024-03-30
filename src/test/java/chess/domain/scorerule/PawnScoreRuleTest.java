9763153ffdc92fa0dadfd159d8320afeff1153f9
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-30 22:01:40 +0900

    test(PawnScoreRule): 같은 줄에 폰이 없는 경우 점수 계산 기능 구현

--- src/test/java/chess/domain/scorerule/PawnScoreRuleTest.java
+++ src/test/java/chess/domain/scorerule/PawnScoreRuleTest.java
@@ -18,4 +18,14 @@ class PawnScoreRuleTest {
                 Position.of(1, 3)
         ))).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("같은 세로 줄에 같은 색의 폰이 하나만 있는 경우, 1점(기본점수)")
+    void findScore() {
+        PawnScoreRule pawnScoreRule = new PawnScoreRule();
+        assertThat(pawnScoreRule.findScore(List.of(
+                Position.of(1, 2),
+                Position.of(2, 3)
+        ))).isEqualTo(2);
+    }
 }
