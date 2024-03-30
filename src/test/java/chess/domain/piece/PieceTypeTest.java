64d80394de7acb4e9d26df8d38dc8bfefec5ba12
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-30 22:52:28 +0900

    test(PieceType): 폰을 제외한 말들의 점수 테스트 작성

--- src/test/java/chess/domain/piece/PieceTypeTest.java
+++ src/test/java/chess/domain/piece/PieceTypeTest.java
@@ -1,6 +1,7 @@
 package chess.domain.piece;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import chess.domain.position.Position;
 import java.util.List;
@@ -9,6 +10,56 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PieceTypeTest {
+
+    @Test
+    @DisplayName("킹은 점수가 없다.")
+    void findScoreKing() {
+        assertAll(
+                () -> assertThat(PieceType.KING.findScore(List.of(Position.of(1, 2), Position.of(2, 2)))).isEqualTo(0),
+                () -> assertThat(PieceType.KING.findScore(List.of(Position.of(1, 2))))
+                        .isEqualTo(0));
+    }
+
+    @Test
+    @DisplayName("퀸은 피스의 수별 9점을 받는다.")
+    void findScoreQueen() {
+        assertAll(
+                () -> assertThat(PieceType.QUEEN.findScore(List.of())).isEqualTo(0),
+                () -> assertThat(PieceType.QUEEN.findScore(List.of(Position.of(1, 2)))).isEqualTo(9),
+                () -> assertThat(PieceType.QUEEN.findScore(List.of(Position.of(1, 2), Position.of(1, 2))))
+                        .isEqualTo(18));
+    }
+
+    @Test
+    @DisplayName("비숍은 피스의 수별 3점을 받는다.")
+    void findScoreBishop() {
+        assertAll(
+                () -> assertThat(PieceType.BISHOP.findScore(List.of())).isEqualTo(0),
+                () -> assertThat(PieceType.BISHOP.findScore(List.of(Position.of(1, 2)))).isEqualTo(3),
+                () -> assertThat(PieceType.BISHOP.findScore(List.of(Position.of(1, 2), Position.of(1, 2))))
+                        .isEqualTo(6));
+    }
+
+    @Test
+    @DisplayName("룩은 피스의 수별 3점을 받는다.")
+    void findScoreRook() {
+        assertAll(
+                () -> assertThat(PieceType.ROOK.findScore(List.of())).isEqualTo(0),
+                () -> assertThat(PieceType.ROOK.findScore(List.of(Position.of(1, 2)))).isEqualTo(5),
+                () -> assertThat(PieceType.ROOK.findScore(List.of(Position.of(1, 2), Position.of(1, 2))))
+                        .isEqualTo(10));
+    }
+
+    @Test
+    @DisplayName("나이트는 피스의 수별 2.5점을 받는다.")
+    void findScoreKnight() {
+        assertAll(
+                () -> assertThat(PieceType.KNIGHT.findScore(List.of())).isEqualTo(0),
+                () -> assertThat(PieceType.KNIGHT.findScore(List.of(Position.of(1, 2)))).isEqualTo(2.5),
+                () -> assertThat(PieceType.KNIGHT.findScore(List.of(Position.of(1, 2), Position.of(1, 2))))
+                        .isEqualTo(5));
+    }
+
     @Nested
     class pawnScore {
 
