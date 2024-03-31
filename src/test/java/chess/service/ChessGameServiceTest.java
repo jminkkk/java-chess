38c9b930b1a0f30558fd2c650f8a107ec5e66237
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-01 03:26:10 +0900

    test(ChessGameService): Fake 객체를 활용하여 Servive 단 로직 테스트

--- src/test/java/chess/service/ChessGameServiceTest.java
+++ src/test/java/chess/service/ChessGameServiceTest.java
@@ -1,4 +1,81 @@
-import static org.junit.jupiter.api.Assertions.*;
+package chess.service;
+
+import static chess.domain.pixture.PieceFixture.BLACK_PAWN;
+import static chess.domain.pixture.PieceFixture.WHITE_PAWN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import chess.domain.board.Board;
+import chess.domain.board.SavedBoardInitializer;
+import chess.domain.board.Turn;
+import chess.domain.piece.Color;
+import chess.domain.position.Position;
+import chess.repository.piece.FakePieceDao;
+import chess.repository.piece.PieceDao;
+import chess.repository.piece.PieceDto;
+import chess.repository.turn.FakeTurnDao;
+import chess.repository.turn.TurnDao;
+import chess.repository.turn.TurnDto;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class ChessGameServiceTest {
-  
+    private ChessGameService chessGameService;
+    private PieceDao pieceDao;
+    private TurnDao turnDao;
+    private Board board;
+
+    @BeforeEach
+    void init() {
+        pieceDao = new FakePieceDao();
+        turnDao = new FakeTurnDao();
+        chessGameService = new ChessGameService(pieceDao, turnDao);
+
+        board = new Board(new SavedBoardInitializer(
+                Map.of(Position.of(1, 1), BLACK_PAWN.getPiece(), Position.of(3, 1), WHITE_PAWN.getPiece())));
+    }
+
+    @Test
+    @DisplayName("현재 Board를 저장한다.")
+    void save() {
+        chessGameService.save(board);
+
+        assertAll(
+                () -> assertThat(pieceDao.findAll()).containsExactly(
+                        PieceDto.of(BLACK_PAWN.getPiece(), Position.of(1, 1)),
+                        PieceDto.of(WHITE_PAWN.getPiece(), Position.of(3, 1))),
+                () -> assertThat(turnDao.findAll()).containsExactly(TurnDto.of(new Turn(Color.BLACK))));
+    }
+
+    @Test
+    @DisplayName("저장된 위치별 피스를 조회한다.")
+    void findAllPiece() {
+        chessGameService.save(board);
+        assertThat(chessGameService.findAllPiece()).containsExactlyInAnyOrderEntriesOf(
+                Map.of(Position.of(1, 1), BLACK_PAWN.getPiece(),
+                        Position.of(3, 1), WHITE_PAWN.getPiece()));
+    }
+
+    @Test
+    @DisplayName("전체 내역을 삭제한다.")
+    void delete() {
+        chessGameService.delete();
+        assertThat(chessGameService.findAllPiece()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("저장된 게임이 있는지 확인한다.")
+    void existGame() {
+        chessGameService.save(board);
+        assertThat(chessGameService.existGame()).isTrue();
+    }
+
+    @Test
+    @DisplayName("저장된 게임이 있는지 확인한다.")
+    void notExistGame() {
+        chessGameService.delete();
+        assertThat(chessGameService.existGame()).isFalse();
+    }
 }
