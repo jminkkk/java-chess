dcb595ed6505a5e940cae915e8ccbffacfe1d8f2
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-03 03:43:28 +0900

    refactor(Pawn): PawnObstacleFinder를 사용하여 움직임을 판단하도록 수정

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,10 +1,27 @@
 package chess.domain.piece;
 
-import chess.domain.piece.attacker.DiagonalAttackPiece;
+import chess.domain.movement.Movement;
+import chess.domain.piece.attacker.ObstacleFinder;
+import chess.domain.piece.attacker.PawnObstacleFinder;
+import chess.domain.position.Position;
+import java.util.Map;
 
-public class Pawn extends DiagonalAttackPiece {
+public class Pawn extends Piece {
+    private final ObstacleFinder obstacleFinder;
 
     public Pawn(final Color color) {
         super(PieceType.PAWN, color);
+        this.obstacleFinder = new PawnObstacleFinder();
+    }
+
+    @Override
+    public boolean canMove(final Position source, final Position target, final Map<Position, Piece> pieces) {
+        return pieceType.getMovements()
+                .stream()
+                .filter(movement -> movement.isSatisfied(color, source,
+                        pieces.getOrDefault(target, Empty.getInstance())))
+                .map(Movement::getDirection)
+                .anyMatch(direction -> direction.canReach(source, target,
+                        obstacleFinder.findObstacle(source, target, pieces)));
     }
 }
