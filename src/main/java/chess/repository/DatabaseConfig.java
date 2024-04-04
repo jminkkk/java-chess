3276d42e04f778d17ebdc2b8026ba6f79a70007e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-04-04 20:32:21 +0900

    refactor(DatabaseConfig): root가 아닌 user라는 유저를 통해 접근하도록 다시 수정
    - docker-compose.yml에도 관련 유저정보 추가

--- src/main/java/chess/repository/DatabaseConfig.java
+++ src/main/java/chess/repository/DatabaseConfig.java
@@ -9,8 +9,8 @@ public final class DatabaseConfig {
     private static final String SERVER = "localhost:13306";
     private static final String DATABASE = "chess";
     private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Seoul";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() {
         try {
